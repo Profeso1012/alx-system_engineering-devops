332b1c46f2088f3e094b8dd5edb8e7a6a789c1d1
Profeso1012 <itzairso204@gmail.com>
2024-06-30 17:15:27 -0700

    dd1

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,30 +1,13 @@
 # Ensure the SSH config directory exists
-file { '/home/ubuntu/.ssh':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0700',
-}
-
-# Ensure the SSH config file exists
-file { '/home/ubuntu/.ssh/config':
-  ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
-  content => '',
-}
-
-# Add configuration to use the private key
-file_line { 'Declare identity file':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
-  require => File['/home/ubuntu/.ssh/config'],  # Ensure the config file is created first
-}
-
-# Add configuration to refuse password authentication
-file_line { 'Turn off passwd auth':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => 'PasswordAuthentication no',
-  require => File['/home/ubuntu/.ssh/config'],  # Ensure the config file is created first
+file { '/etc/ssh/ssh_config':
+  ensure    => 'present'
+  content   => "
+  Host *
+      SendEnv LAN LC_*
+      HashKnownHosts yes
+      GSSAPIAuthentication yes
+      GSSAPIDelegateCredentials no
+      IdentifyFile ~/.ssh/school
+      PasswordAuthentication no
+  "
 }
